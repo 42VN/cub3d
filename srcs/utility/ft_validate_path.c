19c2956493fc5719324df57b0b405380c948e1ad
tranhieutrung <tranhieutrung@outlook.com>
2025-02-18 11:40:38 +0200

    Updated the ft_validate_path.c to check .cub file.

--- srcs/utility/ft_validate_path.c
+++ srcs/utility/ft_validate_path.c
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 09:46:03 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/27 09:46:04 by hitran           ###   ########.fr       */
+/*   Updated: 2025/02/18 11:38:08 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ int	ft_validate_map_path(char *path)
 	if (!path[0] || ft_is_all_white_spaces(path))
 		return (ft_multi_error_ret(path, "Is an empty path", -1));
 	len = ft_strlen(path);
-	if (path[len - 4] != '.' || path[len - 3] != 'c'
-		|| path[len - 2] != 'u' || path[len - 1] != 'b')
+	if (len < 5 || ft_strncmp(path + len - 4, ".cub", 4) != 0)
 		return (ft_multi_error_ret(path, "Not a .cub file", -1));
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
