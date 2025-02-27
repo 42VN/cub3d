3c91f1a2b57452446524fe1a0cb74e6864266461
tranhieutrung <tranhieutrung@outlook.com>
2025-02-27 14:26:41 +0200

    Changed the header files to meet the Norm.

--- includes/asset_manager.h
+++ includes/asset_manager.h
@@ -3,16 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   asset_manager.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:16:08 by ktieu             #+#    #+#             */
-/*   Updated: 2025/02/27 14:09:49 by ktieu            ###   ########.fr       */
+/*   Updated: 2025/02/27 14:18:03 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASSET_MANAGER_H
-#define ASSET_MANAGER_H
-
+# define ASSET_MANAGER_H
 
 # define M_OFFSET	    10
 # define M_PLAYER_SIZE	10
@@ -20,26 +19,13 @@
 # define M_SPACE		"./assets/textures/mini_space.png"
 # define M_WALL			"./assets/textures/mini_wall.png"
 # define M_PLAYER		"./assets/textures/mini_player3.png"
-
-// #ifdef WIDTH
-//  # define M_WIDTH (WIDTH / 5)
-//  # define M_HEIGHT M_WIDTH
-// #elif HEIGHT
-//  # define M_WIDTH (HEIGHT / 5)
-//  # define M_HEIGHT M_WIDTH
-// #else
-//  # define M_WIDTH 200
-//  # define M_HEIGHT 200
-// #endif
-
 # define M_WIDTH 200
 # define M_HEIGHT 200
-
 
 # include "utility.h"
 # include "MLX42.h"
 
-typedef struct s_cub t_cub;
+typedef struct s_cub	t_cub;
 
 //-------------------------------------------------
 // STRUCTS
@@ -49,7 +35,7 @@ typedef enum e_sprite_dir
 {
 	DIR_HORIZONTAL,
 	DIR_VERTICAL
-}	e_sprite_dir;
+}	t_sprite_dir;
 
 typedef struct s_sprite_options
 {
@@ -59,7 +45,6 @@ typedef struct s_sprite_options
 	e_sprite_dir	dir;
 	uint32_t		offset_px;
 }	t_sprite_options;
-
 
 typedef struct s_sprite
 {
@@ -107,6 +92,7 @@ void		am_free(mlx_t *mlx, t_asset_manager *am);
 //-------------------------------------------------
 mlx_image_t	*am_load_png(t_cub *cub, t_png_options options, const char *path);
 // mlx_image_t	*am_load_png_ray(t_cub *cub, const char *path);
-t_sprite	*am_load_sprite(t_cub *c, t_sprite_options options, t_png_options png_options, char *path);
+t_sprite	*am_load_sprite(t_cub *c, t_sprite_options options,
+				t_png_options png_options, char *path);
 
 #endif
