ae3d547d73df643acf3445eb7d0a5f1e36d4e71f
tranhieutrung <tranhieutrung@outlook.com>
2025-02-27 10:01:37 +0200

    Added the clean_rays function to free the rays.

--- srcs/cub3d/free.c
+++ srcs/cub3d/free.c
@@ -3,20 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 17:51:23 by ktieu             #+#    #+#             */
-/*   Updated: 2025/02/21 15:12:13 by ktieu            ###   ########.fr       */
+/*   Updated: 2025/02/27 09:26:43 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	clean_rays(t_ray ***rays)
+{
+	int	i;
+
+	if (!rays || !(*rays))
+		return ;
+	i = 0;
+	while (i < WIDTH)
+	{
+		free((*rays)[i]);
+		(*rays)[i] = NULL;
+		i++;
+	}
+	free(*rays);
+	*rays = NULL;
+}
+
 void	cub3d_free(t_cub *c)
 {
 	if (c)
 	{
+		if (c->mlx != NULL)
+			mlx_close_window(c->mlx);
 		am_free(c->mlx, &c->am);
+		clean_rays(&c->rays);
 		clean_map(&c->map);
 	}
 }
